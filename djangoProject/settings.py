4435be94747c69d01f562aeaa372ff18d831bdc1
evaxrut <136167068+evaxrut@users.noreply.github.com>
2024-05-06 10:24:26 +0000

    Database connection

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -75,9 +75,16 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':
+        { 'ENGINE': 'django.db.backends.postgresql',
+          'NAME': 'postgres',
+          'USER': 'vln2_assignment_groups_34_user',
+          'PASSWORD': 'svjhsnGRAQ',
+          'HOST': 'verklegt-namskeid-ii.northeurope.cloudapp.azure.com',
+          'PORT': '5432',
+          'OPTIONS': {
+              'options': '-c search_path=vln2_assignment_groups_34'
+          }
     }
 }
 
