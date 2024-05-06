883ec710ce26e589e80c86e8bb43c66a01ef23a3
Valli <valgeir02@gmail.com>
2024-05-06 11:28:06 +0000

    Created branch and added applicant views and url redirects

--- SalarySleuth/views.py
+++ SalarySleuth/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def login(request):
+    return HttpResponse("This is the login page.")
+
+
+def logout(request):
+    return HttpResponse("This is the logout page.")
+
+
+def register(request):
+    return HttpResponse("This is the register page.")
+
+
+def listings(request):
+    return HttpResponse("This is the listings page.")
+
+
+def listing_detail(request, lid):
+    return HttpResponse(f"This is the detail view for listing {lid}.")
+
+
+def profile(request, uid):
+    return HttpResponse(f"This is the profile page for user {uid}.")
+
+
+def profile_listings(request, uid):
+    return HttpResponse(f"These are the listings for user {uid}.")
+
+
+def profile_listing_detail(request, uid, lid):
+    return HttpResponse(f"This is the detail view for listing {lid} of user {uid}.")
+
+
+def listing_applicants(request, uid, lid):
+    return HttpResponse(f"These are the applicants for listing {lid} of user {uid}.")
+
+
+def applicant_detail(request, uid, lid, aid):
+    return HttpResponse(f"This is the detail view for applicant {aid} for listing {lid} of user {uid}.")
