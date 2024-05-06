2d87435198a780eca43a70a311e5143b4ec3d8f4
evaxrut <136167068+evaxrut@users.noreply.github.com>
2024-05-06 17:51:33 +0000

    DB update

--- SalarySleuth/admin.py
+++ SalarySleuth/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from SalarySleuth.models import *
 
-# Get all model classes from the models module
-model_classes = [cls for name, cls in vars().items() if isinstance(cls, type)]
+model_classes = [cls for name, cls in vars().items() if isinstance(cls, type) and issubclass(cls, models.Model)]
 
-# Register each model class with the admin site
 for model_class in model_classes:
     admin.site.register(model_class)
--- SalarySleuth/models.py
+++ SalarySleuth/models.py
@@ -1,25 +1,25 @@
 from django.db import models
-#from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator
 import datetime
 
 
 # Create your models here.
 class User(models.Model):
     id = models.AutoField(primary_key=True)
-    SSN = models.CharField(max_length=11,
-                           unique=True)
-    # TODO: validators=[RegexValidator(r'^\d{3}-\d{2}-\d{4}$', 'Enter a valid SSN.')])
+    SSN = models.CharField(max_length=11, unique=True,
+                           validators=[RegexValidator(r'^\d{3}-\d{2}-\d{4}$', 'Enter a valid SSN.')])
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=14, unique=True)
-    authentication_hash = models.CharField(max_length=256, unique=True)
-    # TODO validate
+    authentication_hash = models.CharField(max_length=256, unique=True)  #, validators=['TODO: Add password rules'])
 
     class Meta:
         app_label = 'SalarySleuth'
 
+
 class Applicant(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
     class Meta:
         app_label = 'SalarySleuth'
 
@@ -27,8 +27,10 @@ class Applicant(models.Model):
 class Recruiter(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     company_ssn = models.CharField(max_length=10)
+
     class Meta:
         app_label = 'SalarySleuth'
+
 
 class Company(models.Model):
     company_name = models.CharField(max_length=100)
@@ -39,34 +41,67 @@ class Company(models.Model):
     class Meta:
         app_label = 'SalarySleuth'
 
-class JobListings(models.Model):
+
+class Category(models.Model):
+    field = models.CharField(max_length=100)
+
+
+class EmploymentType(models.Model):
+    type = models.CharField(max_length=100)
+
+
+class JobListing(models.Model):
     id = models.AutoField(primary_key=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    salary_low = models.IntegerField()
+    salary_high = models.IntegerField()
+    recruiter = models.ForeignKey(Recruiter, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    employment_type = models.ForeignKey(EmploymentType, on_delete=models.CASCADE)
 
     class Meta:
         app_label = 'SalarySleuth'
+
+
+class Status(models.Model):
+    type = models.CharField(max_length=100)
+
 
 class Application(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     date = models.DateField(default=datetime.date.today)
-    listing = models.OneToOneField(JobListings, on_delete=models.CASCADE)
+    listing = models.OneToOneField(JobListing, on_delete=models.CASCADE)
     models.UniqueConstraint(fields=['user', 'listing'], name='unique_application')
+    status = models.OneToOneField(Status, on_delete=models.CASCADE)
 
     class Meta:
         app_label = 'SalarySleuth'
 
+
 class Education(models.Model):
+    applicant = models.ForeignKey(Applicant, on_delete=models.CASCADE)
     school = models.CharField(max_length=100)
     level = models.CharField(max_length=100)
     additional_info = models.TextField(max_length=300)
     location = models.CharField(max_length=100)
+
     class Meta:
         app_label = 'SalarySleuth'
+
+
+class Resume(models.Model):
+    pass
+
+    class Meta:
+        app_label = 'SalarySleuth'
+
 
 class Experience(models.Model):
     company_name = models.CharField(max_length=100)
     start_date = models.DateField()
     end_date = models.DateField()
     main_responsibility = models.TextField(max_length=1000)
+
     class Meta:
         app_label = 'SalarySleuth'
 
@@ -75,5 +110,26 @@ class Recommendation(models.Model):
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=7, unique=True)
     company_name = models.CharField(max_length=100)
+
     class Meta:
         app_label = 'SalarySleuth'
+
+
+class ApplicationEducation(models.Model):
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    education = models.ForeignKey(Education, on_delete=models.CASCADE)
+
+
+class ApplicationResume(models.Model):
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE)
+
+
+class ApplicationReccomendations(models.Model):
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    recommendation = models.ForeignKey(Recommendation, on_delete=models.CASCADE)
+
+
+class ApplicationWorkExperience(models.Model):
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    work_experience = models.ForeignKey(Experience, on_delete=models.CASCADE)
