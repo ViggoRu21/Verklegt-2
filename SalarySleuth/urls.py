e8a55c2f9e6d87fdb2d82f1d8c37bbc3179f297e
Glasabarn <viggoh21@ru.is>
2024-05-06 19:29:22 +0000

    animation of Company/Applicant, fixing ernir stuff

--- SalarySleuth/urls.py
+++ SalarySleuth/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='landing'),
+    path('choose', views.choose, name='choose'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('register', views.register, name='register'),
