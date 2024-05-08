11869346000ca8092b5f7c2eb2e3e5e8a8898398
MFernirjons <ernir.jonsson.mymail@gmail.com>
2024-05-08 12:02:30 +0000

    Link functionality restored.

--- applicant/views.py
+++ applicant/views.py
@@ -20,12 +20,13 @@ def register(request):
 
 
 def companies(request):
-    return HttpResponse("This is the list of companies.")
+    #return HttpResponse("This is the list of companies.")
+    return render(request, 'applicant/companies.html')
 
 
 def company_detail(request, cid):
     # return HttpResponse(f"This is the detail view for company {cid}.")
-    return render(request, 'applicant/companies.html', {cid: "dataset"})
+    return render(request, 'applicant/company_detail.html', {cid: "dataset"})
 
 
 def listings(request):
