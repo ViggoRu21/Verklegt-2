e482026472e6ed7c63a84415cc834d4b194bf822
evaxrut <136167068+evaxrut@users.noreply.github.com>
2024-05-08 10:08:17 +0000

    Admin updates + forgot some database stuff

--- applicant/admin.py
+++ applicant/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from utilities_static.models import *
+
+model_classes = [cls for name, cls in vars().items() if isinstance(cls, type) and issubclass(cls, models.Model)]
+
+for model_class in model_classes:
+    admin.site.register(model_class)
