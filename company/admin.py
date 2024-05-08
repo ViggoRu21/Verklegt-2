e703c210290ebb6006c5473fc2337bdaad9944bb
evaxrut <136167068+evaxrut@users.noreply.github.com>
2024-05-08 10:16:48 +0000

    Admin updates + forgot some database stuff

--- company/admin.py
+++ company/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from utilities_static.models import *
+from company.models import *
+from django.apps import apps
+from django.contrib.admin.exceptions import AlreadyRegistered
 
 model_classes = [cls for name, cls in vars().items() if isinstance(cls, type) and issubclass(cls, models.Model)]
 
 for model_class in model_classes:
-    admin.site.register(model_class)
+    try:
+        admin.site.register(model_class)
+    except AlreadyRegistered:
+        print(f"{model_class.__name__} is already registered.")
