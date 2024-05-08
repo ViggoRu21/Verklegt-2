25e49fb83a1d4171efb543933cfa3c74964cb5b0
Viggó <viggoh21@ru.is>
2024-05-08 11:04:24 +0000

    Syntax error on main!

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -90,6 +90,7 @@ DATABASES = {
              'options': '-c search_path=vln2_assignment_groups_34'
          }
          }
+         }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
