741ffe0015a86b2ceacbb0359a1c67e387716794
evaxrut <136167068+evaxrut@users.noreply.github.com>
2024-05-10 09:04:39 +0000

    minor

--- applicant/models.py
+++ applicant/models.py
@@ -10,7 +10,7 @@ class Applicant(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
     def __str__(self) -> str:
-        return f"Applicant: {self.user.first_name} + {self.user.last_name}"
+        return f"Applicant: {self.user.first_name} {self.user.last_name}"
 
     class Meta:
         app_label = 'applicant'
