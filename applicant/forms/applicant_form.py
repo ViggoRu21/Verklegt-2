31dbc22077ce984bc61aa74bc25292d2adc8d13c
evaxrut <136167068+evaxrut@users.noreply.github.com>
2024-05-10 14:30:22 +0000

    forms

--- applicant/forms/applicant_form.py
+++ applicant/forms/applicant_form.py
@@ -4,10 +4,10 @@ from applicant.models import Applicant, Education, Experience, Recommendation
 
 class ApplicantForm(forms.ModelForm):
     class Meta:
+        # TODO finna út hvernig við bætum við inn í user upplýsingar eins og first name, last name af því að það er ekki í þessum klara
         model = Applicant
         fields = ['user', 'applicant_image']
         widgets = {
-            'user': forms.TextInput(attrs={'placeholder': 'Enter user ID'}),
             'applicant_page': forms.FileInput(attrs={'accept': 'image/*'}),
         }
 
