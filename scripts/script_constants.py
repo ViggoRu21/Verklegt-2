2344c8199375623c93d6518b2c4fbc5c078dd38f
evaxrut <136167068+evaxrut@users.noreply.github.com>
2024-05-13 16:41:17 +0000

    randomly generated logos

---
+++ scripts/script_constants.py
@@ -0,0 +1,22 @@
+job_categories = [
+    "Accounting",
+    "Administration",
+    "Human Resources",
+    "Information Technology",
+    "Engineering",
+    "Marketing",
+    "Sales",
+    "Customer Service",
+    "Education",
+    "Healthcare",
+    "Construction",
+    "Logistics",
+    "Technology",
+    "Creative Arts",
+    "Legal",
+    "Science and Research"
+]
+
+employment_types = ['Full Time', 'Part Time', 'Summer Job']
+
+status_types = ['New', 'Maybe', 'Hired', 'Denied', 'Deleted', 'Interview']
