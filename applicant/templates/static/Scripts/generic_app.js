9ec3e6a982abe15bd75d6b2d962789a670a17fe2
MFernirjons <ernir.jonsson.mymail@gmail.com>
2024-05-13 12:44:03 +0000

    Collapsible navigation bar

--- applicant/templates/static/Scripts/generic_app.js
+++ applicant/templates/static/Scripts/generic_app.js
@@ -19,4 +19,20 @@ function testFunction() {
   }
 }
 
+document.getElementById("navbar").onclick = function() {navcollaps()};
+function navcollaps() {
+  // Get the checkbox
+  let checkBox = document.getElementById("navbar");
+  // Get the output text
+
+  // If the checkbox is checked, display the output text
+  if (checkBox = document.getElementById("navbar").checked == true){
+    document.getElementById("sidebar-nav").style.maxHeight = "100%";
+    document.getElementById("sidebar-nav").style.display = visible;
+    
+  } else {
+    document.getElementById("sidebar-nav").style.maxHeight = 0;
+    document.getElementById("sidebar-nav").style.display = hidden;
+  }
+}
 
