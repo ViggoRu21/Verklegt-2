65d12d417c37bda94be76a8374945e5ccfb62280
MFernirjons <116647480+MFernirjons@users.noreply.github.com>
2024-05-16 10:58:27 +0000

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -80,8 +80,9 @@ TEMPLATES = [
         },
     },
 ]
-'''
+
 WSGI_APPLICATION = 'djangoProject.wsgi.application'
+'''
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 '''
