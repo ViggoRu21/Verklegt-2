55bea436aebc33a6b2fc364f26d4e21a6e9bd298
Viggó <viggoh21@ru.is>
2024-05-16 14:11:00 +0000

    Secret

---
+++ company/templates/static/Scripts/generic_comp.js
@@ -0,0 +1,69 @@
+addEventListener("scroll", (event) => {
+    if (document.documentElement.scrollTop > 170) {
+        document.getElementById("search_container").style.position = "fixed";
+        document.getElementById("search_container").style.width = "90%";
+        document.getElementById("search_container").style.top = "0";
+
+    } else {
+        document.getElementById("search_container").style.top = '';
+        document.getElementById("search_container").style.width = "100%";
+        document.getElementById("search_container").style.position = "relative";
+    }});
+
+
+
+function navCollapse() {
+
+  if (document.getElementById("nav_transparent").className === "nav_transparent" ){
+    document.getElementById("nav_transparent").className = "nav";
+    document.getElementById("navbar_container").style.marginTop = "0.6rem";
+    document.getElementById("nav_img").src = nav2;
+  } else {
+    document.getElementById("nav_transparent").className = "nav_transparent";
+    document.getElementById("navbar_container").style.marginTop = "0.5rem";
+    document.getElementById("nav_img").src = nav1;
+  }
+}
+
+
+function profileHover() {
+  document.getElementById("profile_nav").style.visibility = "visible";
+}
+
+
+function profileUnHover() {
+  document.getElementById("profile_nav").style.visibility = "hidden";
+}
+
+
+function testFunction() {
+  let x = document.getElementById("test");
+  if (x.className === "title") {
+    x.className = "title_blue";
+  } else {
+    x.className = "title";
+  }
+}
+
+
+document.getElementById("bigSearch").onchange = function () {
+    let elems = ["search-right-1", "search-right-2", "search-right-5",
+                      "search-right-6", "search-right-7"];
+    for (i in elems) {
+      document.getElementById(elems[i]).className = 'search_hide';
+    }
+    document.getElementById(this[this.selectedIndex].value).className = 'search_show';
+};
+
+
+document.getElementById("bigSelect").onchange = function () {
+    let x = document.getElementById("search-right-3");
+    let y = document.getElementById("search-right-4");
+    if (x.className === "search_show") {
+        x.className = 'search_hide';
+        y.className = 'search_show';
+    } else {
+        y.className = 'search_hide';
+        x.className = 'search_show';
+    }
+};
